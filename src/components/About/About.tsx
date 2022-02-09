e82d36e900cdd0346f26e721d4c8a02fb6294226
unknown <freelancerfathe@gmail.com>
2022-02-09 22:25:03 +0600

    first commit

---
+++ src/components/About/About.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+interface IAppProps {
+}
+
+const About: React.FunctionComponent<IAppProps> = (props) => {
+    return (
+        <h2>About</h2>
+    )
+};
+
+export default About;
